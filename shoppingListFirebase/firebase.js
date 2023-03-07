726713fde2e18f031bad7782bec29f15ea31c32e
UllaMontonen <u.montonen@gmail.com>
2023-03-07 19:14:01 +0200

    added app.config file

--- shoppingListFirebase/firebase.js
+++ shoppingListFirebase/firebase.js
@@ -1,21 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import Constants from 'expo-constants'
+
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBZUbIYMElzZLHTmhaxVBB2KDxXv4TfaCs",
-  authDomain: "shoppinglist-67c2b.firebaseapp.com",
-  projectId: "shoppinglist-67c2b",
-  storageBucket: "shoppinglist-67c2b.appspot.com",
-  messagingSenderId: "563686326684",
-  appId: "1:563686326684:web:ba85238b4fe7242eba8859"
-};
+const firebaseConfig = process.env.EXPO_FIREBASE_CONFIG || Constants.manifest.extra.firebaseConfig;
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-export default database;
+const app = initializeApp(firebaseConfig) 
+export default getDatabase(app);
